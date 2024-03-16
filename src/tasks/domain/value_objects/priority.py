fe996ca050dfe130e2354787d6e6b24ff79476c2
jose.tandavala <jose.tandavala@ideiasdinamicas.com>
2024-03-16 20:36:31 +0100

    :sparkles: (Task) add priority value object

---
+++ src/tasks/domain/value_objects/priority.py
@@ -0,0 +1,14 @@
+from enum import Enum
+
+
+class Priority(Enum):
+    """Represents the priority of task
+
+    Attributes:
+        LOW: The low priority
+        HIGH: The high priority
+        NORMAL: The normal priority
+    """
+    LOW = 'Low'
+    HIGH = 'High'
+    NORMAL = 'Normal'
---
+++ tests/unit/test_priority_enum.py
@@ -0,0 +1,18 @@
+import pytest
+from src.tasks.domain.value_objects.priority import Priority
+
+
+class TestPriority:
+
+    def test_low_priority(self):
+        assert Priority.LOW.value == 'Low'
+
+    def test_high_priority(self):
+        assert Priority.HIGH.value == 'High'
+
+    def test_normal_priority(self):
+        assert Priority.NORMAL.value == 'Normal'
+
+    def test_invalid_priory(self):
+        with pytest.raises(ValueError):
+            Priority('invalid_priority')
