1595f6c1490127b813a7bcccf8ad588af39e2744
jose.tandavala <jose.tandavala@ideiasdinamicas.com>
2024-03-20 05:45:16 +0100

    :sparkles: (Notification) test happy path

---
+++ tests/unit/shared/domain/test_notification.py
@@ -0,0 +1,22 @@
+# sourcery skip: no-loop-in-tests
+import pytest
+
+from src.shared.domain.notification import Notification
+
+class TestNotification:
+    def test_notification_happy_path(self):
+        test_cases = [
+            ([], "", False), # No errors
+            (["Error 1"], "Error 1", True),  # single error
+            (["Error 1", "Error 2"], "Error 1,Error 2", True) # multiple errors
+        ]
+
+        for errors, expected_messages, expected_has_errors in test_cases:
+            notification = Notification()
+            for error in errors:
+                notification.add_error(error)
+            err_messages = notification.messages
+            has_errors = notification.has_errors
+
+            assert err_messages == expected_messages
+            assert has_errors == expected_has_errors
