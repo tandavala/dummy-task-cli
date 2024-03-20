c532b06966143be9daac1e5e91e6fcbdb65c49ed
jose.tandavala <jose.tandavala@ideiasdinamicas.com>
2024-03-20 04:54:02 +0100

    :recycle: (Task) add @dataclass decorator

--- src/tasks/domain/entity/task.py
+++ src/tasks/domain/entity/task.py
@@ -6,18 +6,10 @@ from src.tasks.domain.value_objects.priority import Priority
 
 @dataclass
 class Task:
-
-    def __init__(
-        self,
-        title: str,
-        description: str,
-        due_date: datetime = datetime.now(),
-        priority: Priority = Priority.NORMAL,
-    ) -> None:
-        self.title = title
-        self.description = description
-        self.due_date = due_date
-        self.priority = priority
+    title: str
+    description: str
+    due_date: datetime = datetime.now()
+    priority: Priority = Priority.NORMAL
 
     def set_due_date(self, due_date: datetime):
         self.due_date = due_date
