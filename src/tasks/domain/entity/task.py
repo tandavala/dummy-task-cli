2009800384c0a03acb424d1c0d010240c682ea31
jose.tandavala <jose.tandavala@ideiasdinamicas.com>
2024-03-21 04:54:28 +0100

    :white_check_mark: (Task) add test for task title validation

--- src/tasks/domain/entity/task.py
+++ src/tasks/domain/entity/task.py
@@ -2,17 +2,34 @@ from dataclasses import dataclass
 from datetime import datetime
 
 from src.tasks.domain.value_objects.priority import Priority
+from src.shared.domain.entity import Entity
 
 
 @dataclass
-class Task:
+class Task(Entity):
     title: str
-    description: str
+    description: str = ""
     due_date: datetime = datetime.now()
     priority: Priority = Priority.NORMAL
+
+    def __post_init__(self):
+        self.validate()
 
     def set_due_date(self, due_date: datetime):
         self.due_date = due_date
 
     def set_priority(self, priority: Priority):
         self.priority = priority
+    
+    def validate(self):
+        if len(self.title) > 255:
+            self.notification.add_error("title cannot be longer than 255")
+        
+        if not self.title:
+            self.notification.add_error('title cannot be empty')
+        
+        if len(self.description) > 1024:
+            self.notification.add_error('description cannot be longer than 1024')
+
+        if self.notification.has_errors:
+            raise ValueError(self.notification.messages)
--- tests/unit/task/domain/test_task.py
+++ tests/unit/task/domain/test_task.py
@@ -12,7 +12,7 @@ class TestTask:
         task = Task(title=title, description=description)
         assert task.title == title
         assert task.description == description
-        assert task.due_date.date() == datetime.now().date()
+        assert isinstance(task.due_date, datetime)
         assert task.priority == Priority.NORMAL
 
     def test_can_create_task_with_high_priority(self, task_dto):
@@ -22,7 +22,7 @@ class TestTask:
         task = Task(title, description, priority=Priority.HIGH)
         assert task.title == title
         assert task.description == description
-        assert task.due_date.date() == datetime.now().date()
+        assert isinstance(task.due_date, datetime)
         assert task.priority == Priority.HIGH
 
     def test_can_create_task_with_low_priority(self, task_dto):
@@ -33,4 +33,14 @@ class TestTask:
         assert task.title == title
         assert task.description == description
         assert task.priority == Priority.LOW
-        assert task.due_date.date() == datetime.now().date()
+        assert isinstance(task.due_date, datetime)
+
+    def test_task_title_validation(self):
+        with pytest.raises(ValueError, match="^title cannot be empty$") as excinfo:
+            Task(title="")
+
+
+
+    
+
+    
