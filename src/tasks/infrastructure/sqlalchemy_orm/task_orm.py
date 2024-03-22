5214a19d3a678c03fd01e501fbe830224e4e3d66
jose.tandavala <jose.tandavala@ideiasdinamicas.com>
2024-03-22 05:02:20 +0100

    :card_file_box: (sqlalchemy) setup data layer

--- src/tasks/infrastructure/sqlalchemy_orm/task_orm.py
+++ src/tasks/infrastructure/sqlalchemy_orm/task_orm.py
@@ -8,15 +8,16 @@ from src.tasks.domain.value_objects.priority import Priority
 mapper_registry = registry()
 
 tasks = Table(
-    'tasks',
+    "tasks",
     mapper_registry.metadata,
-    Column('id', Integer, primary_key=True, autoincrement=True),
-    Column('title', String(255)),
-    Column('description', String),
-    Column('due_date', Date),
-    Column('priority', Enum(Priority)),
-    Column('created_at', Date, default=datetime.now())
+    Column("id", Integer, primary_key=True, autoincrement=True),
+    Column("title", String(255)),
+    Column("description", String),
+    Column("due_date", Date),
+    Column("priority", Enum(Priority)),
+    Column("created_at", Date, default=datetime.now()),
 )
+
 
 def start_mappers():
     mapper_registry.map_imperatively(Task, tasks)
--- tests/conftest.py
+++ tests/conftest.py
@@ -1,10 +1,32 @@
 import pytest
 
 from sqlalchemy import create_engine
-from sqlachemy.orm import sessionmaker, clear_mappers
+from sqlalchemy.orm import sessionmaker, clear_mappers
+
+from src.tasks.infrastructure.sqlalchemy_orm.task_orm import (
+    mapper_registry,
+    start_mappers,
+)
+
+
+@pytest.fixture
+def task_dto():
+    return {"title": "Task # 1", "description": "Description task # 1"}
+
 
 @pytest.fixture
 def in_memory_db():
-    engine = create_engine('sqlite:///:memory:')
+    engine = create_engine("sqlite:///:memory:")
+    mapper_registry.metadata.create_all(engine)
 
 
+@pytest.fixture
+def session_factory(in_memory_db):
+    start_mappers()
+    yield sessionmaker(bind=in_memory_db)
+    clear_mappers()
+
+
+@pytest.fixture
+def session(session_factory):
+    return session_factory()
